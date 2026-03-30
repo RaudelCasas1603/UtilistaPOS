1f556c69e785e878f4bad68d6c340f295ab0d0d1
Raudel Casas Cuevas <rcasas@bansi.com.mx>
2026-03-30 13:50:41 -0600

    En proceso de crear la Pantalla de admin de categorias

--- app/configuracion/admin-categorias/page.tsx
+++ app/configuracion/admin-categorias/page.tsx
@@ -1,5 +1,32 @@
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function adminCategorias(){
     return(
-        <div>hola mundo</div>
+        <>
+        <div>
+        <h2 className="text-2xl font-bold">Administracion de Categorias</h2>
+        <span className="text-lg">En esta ventana podras crear, editar, modificar o eliminar las categorias de los productos que manejas</span>
+        </div>
+
+        {/**Contenedor de las categorias */}
+        <div>
+
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    
+                </CardTitle>
+            </CardHeader>
+        </Card>
+
+        </div>
+        </>
     )
 }
